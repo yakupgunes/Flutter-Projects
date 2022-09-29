5d4d5768e163602e782b5d6af228bbecd6030741
Yakup Güneş <yyakupgunes@gmail.com>
2022-09-29 18:08:26 +0300

    Examples

---
+++ flutter_projects/lib/FirstExample/EggExamp/egg.Variables.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_projects/FirstExample/EggExamp/egg.Class.dart';
+
+Map<String, List<SavedEggs>> views = {
+  'sm': [],
+  "md": [],
+  "lr": [],
+};
--- flutter_projects/lib/main.dart
+++ flutter_projects/lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_projects/denemeler/week2/egg.dart';
+import 'package:flutter_projects/FirstExample/EggExamp/egg.dart';
 
 void main() => runApp(EggSeller());
 
