0387a08d5db5a33fc55b031b43752d20574aec8e
Yakup Güneş <yyakupgunes@gmail.com>
2022-10-24 18:12:40 +0300

    reupdate

--- flutter_projects/lib/denemeler/week1/TestApp_const.dart
+++
@@ -1,6 +0,0 @@
-// ignore_for_file: file_names
-
-import 'package:flutter/material.dart';
-
-const Icon dogruIconu = Icon(Icons.mood, color: Colors.green);
-const Icon yanlisIconu = Icon(Icons.mood_bad, color: Colors.red);
--- flutter_projects/lib/denemeler/week2/egg.Variables.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_projects/denemeler/week2/egg.Class.dart';
-
-Map<String, List<SavedEggs>> views = {
-  'sm': [],
-  "md": [],
-  "lr": [],
-};
